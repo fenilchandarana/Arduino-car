d3e072a0ffa96343caf46ffd12288c87e972c318
fenil <fenil.c999@gmail.com>
2024-05-23 02:09:12 +0530

    l298n changed

--- programming/individual components/esp32/l298n/l298n.ino
+++ programming/individual components/esp32/l298n/l298n.ino
@@ -15,7 +15,7 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  
+
   // Turn off motors - Initial state
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
@@ -26,36 +26,53 @@ void setup() {
 void loop() {
   directionControl();
   delay(1000);
-//  speedControl();
-//  delay(1000);
+  //  speedControl();
+  //  delay(1000);
 }
 
 // This function lets you control spinning direction of motors
 void directionControl() {
   // Set motors to maximum speed
   // For PWM maximum possible values are 0 to 255
-  analogWrite(enA, 255);
-  analogWrite(enB, 255);
+  analogWrite(enA, 100);
+  analogWrite(enB, 100);
 
   // Turn on motor A & B
+  // Move forward
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   delay(2000);
-  
+
   // Now change motor directions
+  // Move backward
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   delay(2000);
-  
+
   // Turn off motors
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
+
+  // Move right
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  delay(2000);
+
+  // Move left
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(2000);
+
 }
 
 // This function lets you control speed of the motors
@@ -65,21 +82,21 @@ void speedControl() {
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  
+
   // Accelerate from zero to maximum speed
   for (int i = 0; i < 256; i++) {
     analogWrite(enA, i);
     analogWrite(enB, i);
     delay(20);
   }
-  
+
   // Decelerate from maximum speed to zero
   for (int i = 255; i >= 0; --i) {
     analogWrite(enA, i);
     analogWrite(enB, i);
     delay(20);
   }
-  
+
   // Now turn off motors
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
